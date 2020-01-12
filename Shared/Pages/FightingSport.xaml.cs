a1a6efa00da78604c7788c4880694bc1a9d2b7b4
maabb <32270131+maabb@users.noreply.github.com>
2020-01-12 23:32:17 +0300

    Update FightingSport.xaml.cs

--- Shared/Pages/FightingSport.xaml.cs
+++ Shared/Pages/FightingSport.xaml.cs
@@ -12,15 +12,7 @@ namespace MobileAppHW.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FightingSport : ContentPage
     {
-        private List<DataF> ItemsF = new List<DataF>()
-        {
-            new DataF{Name="Karate", Detail="Karate means empty hand . This form of warfare originated on Japan's island of Okinawa. Karate is now generally a general term for all groups of Japanese martial arts. Karate handles offensive and defensive movements and avoids grappling and wrestling. Students are taught to strike fast and powerful blows almost anywhere in the body, including dangerous kicks with the legs. Karate also consists of hard and soft styles. Some schools teach  full contact  karate, where their students use protective equipment to soften the blows of real combat."
-            , Fimage="SD"},
-
-
-
-        };
-
+       
 
 
         public FightingSport()
