c1e97bb714159525224be61edf37d0f1950536ae
maabb <32270131+maabb@users.noreply.github.com>
2020-01-12 23:53:21 +0300

    Add files via upload

---
+++ Shared/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileAppHW
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var navPage = new NavigationPage(new MobileAppHW.MainPage());
+            MainPage = navPage;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
